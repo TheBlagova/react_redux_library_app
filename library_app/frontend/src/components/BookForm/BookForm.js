89c16f640ced34471e3bdee6d81f333cfd95ecf6
TheBlagova <theblagova@gmail.com>
2024-11-21 13:28:17 +0300

    css styles

---
+++ library_app/frontend/src/components/BookForm/BookForm.js
@@ -0,0 +1,11 @@
+import './BookForm.css';
+
+function BookForm() {
+  return (
+    <div classname="app-block book-form">
+      <h2>Add a new book</h2>
+    </div>
+  );
+}
+
+export default BookForm;
---
+++ library_app/frontend/src/components/BookList/BookList.js
@@ -0,0 +1,11 @@
+import './BookList.css';
+
+function BookList() {
+  return (
+    <div classname="app-block book-list">
+      <h2>Book list</h2>
+    </div>
+  );
+}
+
+export default BookList;
