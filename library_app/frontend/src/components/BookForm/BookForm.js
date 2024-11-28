836fe14f63347e82bacdb704d037873e6bcf9cf7
TheBlagova <theblagova@gmail.com>
2024-11-28 14:11:22 +0300

    added thunkFunction

--- library_app/frontend/src/components/BookForm/BookForm.js
+++ library_app/frontend/src/components/BookForm/BookForm.js
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { useDispatch } from 'react-redux';
-import axios from 'axios';
+import { thunkfunction } from '../../redux/slices/booksSlice';
 import { addBook } from '../../redux/slices/booksSlice';
 import booksData from '../../Data/books.json';
 import createBookWithId from '../../utils/createBookWithId';
@@ -28,16 +28,8 @@ function BookForm() {
     }
   };
 
-  const handleAddRandomBookViaAPI = async () => {
-    try {
-      const res = await axios.get('http://localhost:4000/random-book');
-
-      if (res.data && res.data.title && res.data.author) {
-        dispath(addBook(createBookWithId(res.data, 'API')));
-      }
-    } catch (error) {
-      console.log('Error fetching random book', error);
-    }
+  const handleAddRandomBookViaAPI = () => {
+    dispath(thunkfunction);
   };
 
   return (
